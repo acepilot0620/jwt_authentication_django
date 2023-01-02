83bf1c405deb91ddbdabd57f9ecc3b05b2a01252
acepilot0620 <>
2023-01-02 23:21:31 +0900

    sign_up, login_ logout complete

--- assignment/user/models.py
+++ assignment/user/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-from django.core.validators import RegexValidator
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-from django.utils import timezone
 
 from .mixin import TimedModelMixin
 from .user_manager import UserManager
-
 
 class User(AbstractUser, TimedModelMixin):
     username = None
@@ -16,9 +12,6 @@ class User(AbstractUser, TimedModelMixin):
     REQUIRED_FIELDS = []
     objects = UserManager()
     name = models.CharField(max_length=64, default='', null=True, blank=True, verbose_name='이름')
-    phoneNumberRegex = RegexValidator(regex=r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
-    phone = models.CharField(validators=[phoneNumberRegex], max_length=11, unique=True, verbose_name='전화 번호')
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
 
     class Meta:
         verbose_name = "사용자 정보"
